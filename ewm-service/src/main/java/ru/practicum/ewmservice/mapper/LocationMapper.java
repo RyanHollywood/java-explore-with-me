4c9989552d76e6589a9dfecc7c7006d530f9555b
Alex Savelev <ryan.hollywood@yandex.ru>
2022-10-04 14:02:29 +0300

    refactor: LocationMapper added

---
+++ ewm-service/src/main/java/ru/practicum/ewmservice/mapper/LocationMapper.java
@@ -0,0 +1,20 @@
+package ru.practicum.ewmservice.mapper;
+
+import ru.practicum.ewmservice.dto.location.LocationDto;
+import ru.practicum.ewmservice.model.Location;
+
+public class LocationMapper {
+    public static Location fromLocationDto(LocationDto locationDto) {
+        return Location.builder()
+                .lat(locationDto.getLat())
+                .lon(locationDto.getLon())
+                .build();
+    }
+
+    public static LocationDto toLocationDto(Location location) {
+        return LocationDto.builder()
+                .lat(location.getLat())
+                .lon(location.getLon())
+                .build();
+    }
+}
--- ewm-service/src/main/java/ru/practicum/ewmservice/model/Location.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/model/Location.java
@@ -1,11 +1,11 @@
-package ru.practicum.ewmservice.dto.location;
+package ru.practicum.ewmservice.model;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class LocationDto {
+public class Location {
     private float lat;
     private float lon;
 }
