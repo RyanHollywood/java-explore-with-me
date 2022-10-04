05130a699ec15f0c875a460a9490abbef1db197a
Alex Savelev <ryan.hollywood@yandex.ru>
2022-10-04 20:43:17 +0300

    refactor: create category method ready

--- ewm-service/src/main/java/ru/practicum/ewmservice/service/adminewm/AdminServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/service/adminewm/AdminServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.practicum.ewmservice.service.adminewm;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewmservice.dto.category.CategoryDto;
 import ru.practicum.ewmservice.dto.category.NewCategoryDto;
@@ -9,11 +11,23 @@ import ru.practicum.ewmservice.dto.event.AdminUpdateEventRequestDto;
 import ru.practicum.ewmservice.dto.event.EventFullDto;
 import ru.practicum.ewmservice.dto.user.NewUserRequest;
 import ru.practicum.ewmservice.dto.user.UserDto;
+import ru.practicum.ewmservice.mapper.CategoryMapper;
+import ru.practicum.ewmservice.model.Category;
+import ru.practicum.ewmservice.storage.CategoryRepository;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class AdminServiceImpl implements AdminService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public AdminServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @Override
     public List<EventFullDto> getEvents() {
         return null;
@@ -41,11 +55,15 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public CategoryDto createCategory(NewCategoryDto newCategoryDto) {
-        return null;
+        Category newCategory = CategoryMapper.fromNewCategoryDto(newCategoryDto);
+        newCategory = categoryRepository.save(newCategory);
+        log.debug("NEW CATEGORY {} CREATED", newCategory.getName());
+        return CategoryMapper.toCategoryDto(newCategory);
     }
 
     @Override
     public void deleteCategory(long catId) {
+        categoryRepository.deleteById(catId);
     }
 
     @Override
