dc58a9f6400cdc552f3bef551d7d0b9849f3dcdb
Alex Savelev <ryan.hollywood@yandex.ru>
2022-10-06 19:37:49 +0300

    refactor: ewm-service locations, events tables added

--- ewm-service/src/main/resources/schema.sql
+++ ewm-service/src/main/resources/schema.sql
@@ -12,3 +12,32 @@ CREATE TABLE IF NOT EXISTS users
     email TINYTEXT,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS locations
+(
+    id INT8 NOT NULL AUTO_INCREMENT,
+    lat FLOAT,
+    lon FLOAT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS events
+(
+    id INT8 NOT NULL AUTO_INCREMENT,
+    annotation TINYTEXT,
+    category_id INT8,
+    create_on DATETIME,
+    confirmedRequests INT8,
+    description MEDIUMTEXT,
+    eventDate DATETIME,
+    initiator_id INT8,
+    location_id INT8,
+    paid BOOLEAN,
+    participantLimit INT4,
+    publishedOn TINYTEXT,
+    requestModeration BOOLEAN,
+    state TINYTEXT,
+    title TINYTEXT,
+    views TINYTEXT,
+    PRIMARY KEY (id)
+);
