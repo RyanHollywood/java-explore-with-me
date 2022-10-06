143331808baabb0caad97903c7b7ae38e2fe1b5a
Alex Savelev <ryan.hollywood@yandex.ru>
2022-10-06 20:18:11 +0300

    refactor: ewm-service events state emun, need FKeys in events table

--- ewm-service/src/main/java/ru/practicum/ewmservice/model/Event.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/model/Event.java
@@ -1,27 +1,70 @@
 package ru.practicum.ewmservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "events")
 public class Event {
+    @Column
     private String annotation;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
+
+    @Column
     private long confirmedRequests;
+
+    @Column
     private LocalDateTime createOn;
+
+    @Column
     private String description;
+
+    @Column
     private LocalDateTime eventDate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "initiator_id", referencedColumnName = "id")
     private User initiator;
+
+    @OneToOne
+    @JoinColumn(name = "location_id", referencedColumnName = "id")
     private Location location;
+
+    @Column
     private boolean paid;
+
+    @Column
     private int participantLimit;
+
+    @Column
     private String publishedOn;
+
+    @Column
     private boolean requestModeration;
-    private String state;
+
+    @Column
+    @Enumerated(EnumType.ORDINAL)
+    private EventState state;
+
+    @Column
     private String title;
+
+    @Column
     private long views;
 }
--- ewm-service/src/main/java/ru/practicum/ewmservice/model/Location.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/model/Location.java
@@ -1,11 +1,26 @@
 package ru.practicum.ewmservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "locations")
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
     private float lat;
+
+    @Column
     private float lon;
 }
